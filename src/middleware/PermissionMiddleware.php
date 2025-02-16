a34681cfbbe3df6b42994ccfd784719f7e5a8bc2
落月 <1569097443@qq.com>
2025-02-16 21:46:16 +0800

    跟换认证框架

--- src/middleware/PermissionMiddleware.php
+++ src/middleware/PermissionMiddleware.php
@@ -2,23 +2,108 @@
 
 namespace Luoyue\WebmanMvcCore\middleware;
 
+use Casbin\WebmanPermission\Permission;
+use Luoyue\WebmanMvcCore\annotation\authentication\Anonymous;
+use Luoyue\WebmanMvcCore\annotation\authorization\hasPermi;
+use Luoyue\WebmanMvcCore\annotation\authorization\hasRole;
 use Luoyue\WebmanMvcCore\exception\PermissionException;
-use Luoyue\WebmanMvcCore\handler\PermissionHandler;
-use support\Container;
+use Luoyue\WebmanMvcCore\exception\UserException;
+use ReflectionAttribute;
+use ReflectionMethod;
 use Webman\Http\Request;
 use Webman\Http\Response;
-use Webman\MiddlewareInterface;
+use WebmanTech\Auth\Interfaces\IdentityInterface;
 
-class PermissionMiddleware implements MiddlewareInterface
+class PermissionMiddleware extends \WebmanTech\Auth\Middleware\Authentication
 {
+
+    protected array $config;
+
+    public function __construct()
+    {
+        $this->config = config('plugin.luoyue.webman-mvc-core.app.permission');
+    }
 
     public function process(Request $request, callable $handler): Response
     {
-        /** @var PermissionHandler $service */
-        $service = Container::get(PermissionHandler::class);
-        if(!$service->process($request)) {
-            throw new PermissionException('您没有权限访问');
+        if (!$this->config['enable'] ?? true) {
+            return $handler($request);
         }
-        return $handler($request);
+        $anonymous = $this->filterAnnotation($this->getAttributes(), Anonymous::class);
+        if ($anonymous) {
+            return $handler($request);
+        }
+
+        $guard = $this->getGuard();
+        $identity = $guard->getAuthenticationMethod()->authenticate($request);
+        if ($identity instanceof IdentityInterface) {
+            $guard->login($identity);
+            if ($this->hasPermission()) {
+                return $handler($request);
+            }else{
+                throw new PermissionException('permission denied');
+            }
+        }
+        if ($this->isOptionalRoute($request)) {
+            return $handler($request);
+        }
+
+        return $guard->getAuthenticationFailedHandler()->handle($request);
     }
+
+    private function hasPermission(): bool
+    {
+        $userId = (string)$this->getGuard()->getId();
+        if (!$userId) {
+            throw new UserException('user is not User');
+        }
+        $attributes = $this->getAttributes();
+        $hasPermiAttributes = $this->filterAnnotation($attributes, hasPermi::class);
+        //遍历注解
+        foreach ($hasPermiAttributes as $hasPermiAttribute) {
+            //遍历权限
+            foreach ((array)$hasPermiAttribute->getArguments()[0] as $prmi) {
+                $prmi = explode(':', $prmi);
+                //权限或
+                if (Permission::hasPermissionForUser($userId, ...$prmi)) {
+                    return true;
+                }
+            }
+        }
+
+        $hasRoleAttributes = $this->filterAnnotation($attributes, hasRole::class);
+        foreach ($hasRoleAttributes as $hasRoleAttribute) {
+            foreach ((array)$hasRoleAttribute->getArguments()[0] as $role) {
+                $role = explode(':', $role);
+                if (Permission::hasRoleForUser($userId, ...$role)) {
+                    return true;
+                }
+            }
+        }
+        return !$hasPermiAttributes && !$hasRoleAttributes;
+    }
+
+    /**
+     * @return ReflectionAttribute[]
+     * @throws \ReflectionException
+     */
+    private function getAttributes(): array
+    {
+        return (new ReflectionMethod(request()->controller, request()->action))->getAttributes();
+    }
+
+
+    /**
+     * 过滤注解
+     * @param ReflectionAttribute[] $attributes
+     * @param string $annotationName
+     * @return array
+     */
+    private function filterAnnotation(array $attributes, string $annotationName): array
+    {
+        return array_filter($attributes, function (ReflectionAttribute $attribute) use ($annotationName) {
+            return $attribute->getName() === $annotationName;
+        });
+    }
+
 }
